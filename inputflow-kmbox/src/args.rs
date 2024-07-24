7bc226d958faff8280c9c06f5527117b5b9c8dfc
ConnorBP <connor.postma@gmail.com>
2024-07-24 10:51:10 -0400

    Extended kmbox init args and added defaults

--- inputflow-kmbox/src/args.rs
+++ inputflow-kmbox/src/args.rs
@@ -1,11 +1,57 @@
 // Arguments for the plugin initialization
 
+use std::default;
 use ::std::error::Error;
+
+// default config values for each platform
+
+/// Returns the default value for serial port name or path
+/// on windows this is generally `COM#`
+/// on unix this is generally a path like `/dev/ttyUSB#`
+fn default_com_port() -> String {
+    #[cfg(target_family="unix")]
+    {"/dev/ttyUSB0".to_string()}
+    #[cfg(target_family="windows")]
+    {"COM8".to_string()}
+}
+
+/// Returns the default serial device name to look for
+/// this is used when auto port find mode is enabled instead
+/// specifying of manual com path
+fn default_device_name() -> String {
+    "USB-SERIAL CH340".to_string()
+}
+
+/// Auto port selection by device name is the default behaviour
+fn default_auto_select() -> bool {
+    true
+}
+
+/// Default serial baud rate is the default of KMBox:
+/// one hundred and fifteen thousand, two hundred.
+fn default_baud_rate() -> u32 {
+    115_200
+}
 
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 pub(crate) struct Args {
+    /// Automatically selects the serial port
+    /// based on the device name argument if true
+    #[serde(default = "default_auto_select")]
+    pub auto_select: bool,
+    /// The device name to search for in auto mode.
+    /// defaults to `USB-SERIAL CH340`.
+    #[serde(default = "default_device_name")]
+    pub device_name: String,
+    /// The serial port path to connect to (used when auto_select is false)
+    /// defaults to `COM8` on Windows and `/dev/ttyUSB0` on Unix
+    #[serde(default = "default_com_port")]
     pub com_port: String,
+    #[serde(default = "default_baud_rate")]
     pub baud_rate: u32,
+    /// Port timeout value in milliseconds.
+    /// Defaults to zero (no timeout)
+    #[serde(default)]
     pub timeout_ms: u64,
 }
 
